a9e7c2c71e61e5f0a84950d6addfb3ff1c0edd16
Evandro Lima <evandro.lima@gat.digital>
2022-08-16 11:43:55 +0000

    Feature latest posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
-import { Col, Row, Typography } from 'antd';
+import { Col, Divider, Row, Space, Typography } from 'antd';
 import CompanyMetrics from './app/features/CompanyMetrics';
+import LatestPosts from './app/features/LatestPosts';
 
 const { Title, Paragraph } = Typography;
 
 function App() {
   return (
-    <Row>
-      <Col span={24}>
-        <Title level={2}>Olá, José Sousa</Title>
-        <Paragraph>
-          Este é o resumo da empresa nos últimos doze meses
-        </Paragraph>
-      </Col>
-      <Col span={24}>
-        <CompanyMetrics />
-      </Col>
-    </Row>
+    <Space
+      direction='vertical'
+      size='small'
+      style={{ maxWidth: '100%' }}
+    >
+      <Row>
+        <Col span={24}>
+          <Title level={2}>Olá, José Sousa</Title>
+          <Paragraph>
+            Este é o resumo da empresa nos últimos doze
+            meses
+          </Paragraph>
+        </Col>
+        <Col span={24}>
+          <CompanyMetrics />
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col span={24}>
+          <Title level={3}>Últimos posts</Title>
+        </Col>
+        <Col span={24}>
+          <LatestPosts />
+        </Col>
+      </Row>
+    </Space>
   );
 }
 
