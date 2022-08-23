463591f6cd4cc7419fb7f8e40cbc0c9d9019c90d
Evandro Lima <evandro.lima@gat.digital>
2022-08-23 09:38:51 +0000

    Add logo in header

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -1,24 +1,19 @@
-import { Menu, Layout, MenuProps } from 'antd';
+import { Avatar, Layout, Row } from 'antd';
+import logo from '../../../assets/logo.svg';
 
 const { Header } = Layout;
-
-const items1: MenuProps['items'] = ['1', '2', '3'].map(
-  (key) => ({
-    key,
-    label: `nav ${key}`,
-  })
-);
 
 export default function DefaultLayoutHeader() {
   return (
     <Header className='header'>
-      <div className='logo' />
-      <Menu
-        theme='dark'
-        mode='horizontal'
-        defaultSelectedKeys={['2']}
-        items={items1}
-      />
+      <Row
+        justify='space-between'
+        style={{ height: '100%' }}
+        align='middle'
+      >
+        <img src={logo} alt={'AlgaNews Admin'} />
+        <Avatar />
+      </Row>
     </Header>
   );
 }
