f39d39e4ba7da1f6a761bd1ac34d6c06bf297892
Evandro Lima <evandro.lima@gat.digital>
2022-08-23 14:38:47 +0000

    Feature user-list

---
+++ src/app/features/UserList.tsx
@@ -0,0 +1,119 @@
+import {
+  Avatar,
+  Button,
+  Space,
+  Switch,
+  Table,
+  Tag,
+  Typography,
+} from 'antd';
+import { format } from 'date-fns';
+import { User } from 'goodvandro-alganews-sdk';
+import { useEffect } from 'react';
+import useUsers from '../../core/hooks/useUsers';
+import {
+  EyeOutlined,
+  EditOutlined,
+} from '@ant-design/icons';
+
+export default function UserList() {
+  const { users, fetchUsers } = useUsers();
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  return (
+    <>
+      <Table<User.Summary>
+        dataSource={users}
+        columns={[
+          {
+            dataIndex: 'name',
+            title: 'Nome',
+            width: 200,
+            render(name: string, row) {
+              return (
+                <Space>
+                  <Avatar
+                    size='small'
+                    src={row.avatarUrls.small}
+                  />
+                  <Typography.Text
+                    style={{ maxWidth: '100%' }}
+                  >
+                    {name}
+                  </Typography.Text>
+                </Space>
+              );
+            },
+          },
+          {
+            dataIndex: 'email',
+            title: 'E-mail',
+            ellipsis: true,
+            width: 200,
+          },
+          {
+            dataIndex: 'role',
+            title: 'Perfil',
+            align: 'center',
+            render(role) {
+              return (
+                <Tag
+                  color={
+                    role === 'MANAGER' ? 'red' : 'blue'
+                  }
+                >
+                  {role === 'EDITOR'
+                    ? 'Editor'
+                    : role === 'MANAGER'
+                    ? 'Gestor'
+                    : 'Assistente'}
+                </Tag>
+              );
+            },
+          },
+          {
+            dataIndex: 'createdAt',
+            title: 'Criação',
+            align: 'center',
+            render(createdAt: string) {
+              return format(
+                new Date(createdAt),
+                'dd/MM/yyyy'
+              );
+            },
+          },
+          {
+            dataIndex: 'active',
+            title: 'Ativo',
+            align: 'center',
+            render(active: boolean) {
+              return <Switch defaultChecked={active} />;
+            },
+          },
+          {
+            dataIndex: 'id',
+            title: 'Ações',
+            align: 'center',
+            render() {
+              return (
+                <>
+                  <Button
+                    size='small'
+                    icon={<EyeOutlined />}
+                  />
+                  <Button
+                    size='small'
+                    icon={<EditOutlined />}
+                  />
+                </>
+              );
+            },
+          },
+        ]}
+      />
+    </>
+  );
+}
