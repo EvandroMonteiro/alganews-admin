2ab581847348c10b50ef0aec2c1f9bdd7b2d7044
Evandro Lima <evandro.lima@gat.digital>
2022-08-23 10:26:05 +0000

    Customize default layout

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -8,7 +8,11 @@ export default function DefaultLayoutHeader() {
     <Header className='header'>
       <Row
         justify='space-between'
-        style={{ height: '100%' }}
+        style={{
+          height: '100%',
+          maxWidth: 1190,
+          margin: '0 auto',
+        }}
         align='middle'
       >
         <img src={logo} alt={'AlgaNews Admin'} />
