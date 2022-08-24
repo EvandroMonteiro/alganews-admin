a5251749a31f731eb3aaed77051959b6e5da477e
Evandro Lima <evandro.lima@gat.digital>
2022-08-24 11:53:12 +0000

    Remove paginate for now

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -21,7 +21,7 @@ import {
 import { ColumnProps } from 'antd/lib/table';
 
 export default function UserList() {
-  const { users, fetchUsers, toggleUserStatus } =
+  const { users, fetchUsers, toggleUserStatus, fetching } =
     useUsers();
 
   useEffect(() => {
@@ -88,7 +88,9 @@ export default function UserList() {
   return (
     <>
       <Table<User.Summary>
+        loading={fetching}
         dataSource={users}
+        pagination={false}
         columns={[
           {
             dataIndex: 'name',
