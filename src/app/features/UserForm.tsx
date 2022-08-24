465f5dd020342147770a21b0543602e135d02fd4
Evandro Lima <evandro.lima@gat.digital>
2022-08-24 18:04:01 +0000

    Mount form object

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -8,24 +8,70 @@ import {
   Divider,
   Select,
   Tabs,
+  Upload,
+  Button,
 } from 'antd';
-import React from 'react';
+import { FileService } from 'goodvandro-alganews-sdk';
+import React, { useCallback, useState } from 'react';
+import { UserOutlined } from '@ant-design/icons';
+import ImageCrop from 'antd-img-crop';
 
 const { TabPane } = Tabs;
 
 export default function UserForm() {
+  const [avatar, setAvatar] = useState('');
+
+  const handleAvatarUpload = useCallback(
+    async (file: File) => {
+      const avatarSource = await FileService.upload(file);
+      setAvatar(avatarSource);
+    },
+    []
+  );
+
   return (
-    <Form layout={'vertical'}>
+    <Form
+      layout={'vertical'}
+      onFinish={(form) => {
+        console.log(form);
+      }}
+    >
       <Row gutter={24} align={'middle'}>
         <Col lg={4}>
-          <Avatar size={128} />
+          <ImageCrop
+            rotate
+            shape={'round'}
+            grid
+            aspect={1 / 1}
+          >
+            <Upload
+              maxCount={1}
+              onRemove={() => {
+                setAvatar('');
+              }}
+              beforeUpload={async (file) => {
+                await handleAvatarUpload(file);
+                return false;
+              }}
+            >
+              <Avatar
+                src={avatar}
+                size={128}
+                icon={<UserOutlined />}
+                style={{ cursor: 'pointer' }}
+              />
+            </Upload>
+          </ImageCrop>
         </Col>
 
         <Col lg={10}>
-          <Form.Item label={'Nome'}>
+          <Form.Item label={'Nome'} name={'name'}>
             <Input placeholder={'E.g.: João Silva'} />
           </Form.Item>
-          <Form.Item label={'Data de nascimento'}>
+          <Form.Item
+            label={'Data de nascimento'}
+            name={'birthdate'}
+          >
             <DatePicker
               style={{ width: '100%' }}
               format={'DD/MM/YYYY'}
@@ -34,7 +80,7 @@ export default function UserForm() {
         </Col>
 
         <Col lg={10}>
-          <Form.Item label={'Bio'}>
+          <Form.Item label={'Bio'} name={'bio'}>
             <Input.TextArea rows={5} />
           </Form.Item>
         </Col>
@@ -44,7 +90,7 @@ export default function UserForm() {
         </Col>
 
         <Col lg={12}>
-          <Form.Item label={'Perfil'}>
+          <Form.Item label={'Perfil'} name={'role'}>
             <Select placeholder={'Selecione um perfil'}>
               <Select.Option value={'EDITOR'}>
                 Editor
@@ -60,7 +106,7 @@ export default function UserForm() {
         </Col>
 
         <Col lg={12}>
-          <Form.Item label={'Email'}>
+          <Form.Item label={'Email'} name={'email'}>
             <Input
               type='email'
               placeholder={'E.g.: contato@joao.silva'}
@@ -80,36 +126,54 @@ export default function UserForm() {
             >
               <Row gutter={24}>
                 <Col lg={8}>
-                  <Form.Item label={'País'}>
+                  <Form.Item
+                    label={'País'}
+                    name={['location', 'country']}
+                  >
                     <Input placeholder={'E.g.: Brasil'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Estado'}>
+                  <Form.Item
+                    label={'Estado'}
+                    name={['location', 'state']}
+                  >
                     <Input
                       placeholder={'E.g.: Espírito Santo'}
                     />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Cidade'}>
+                  <Form.Item
+                    label={'Cidade'}
+                    name={['location', 'city']}
+                  >
                     <Input placeholder={'E.g.: Vitória'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Telefone'}>
+                  <Form.Item
+                    label={'Telefone'}
+                    name={'phone'}
+                  >
                     <Input
                       placeholder={'(27) 99999-0000'}
                     />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'CPF'}>
+                  <Form.Item
+                    label={'CPF'}
+                    name={'taxpayerId'}
+                  >
                     <Input placeholder={'111.222.333-44'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Preço por palavra'}>
+                  <Form.Item
+                    label={'Preço por palavra'}
+                    name={'pricePerWord'}
+                  >
                     <Input placeholder={'0'} />
                   </Form.Item>
                 </Col>
@@ -119,7 +183,10 @@ export default function UserForm() {
                     return (
                       <React.Fragment key={index}>
                         <Col lg={6}>
-                          <Form.Item label={'Habilidade'}>
+                          <Form.Item
+                            label={'Habilidade'}
+                            name={['skills', index, 'name']}
+                          >
                             <Input
                               placeholder={
                                 'E.g.: Javascript'
@@ -128,7 +195,14 @@ export default function UserForm() {
                           </Form.Item>
                         </Col>
                         <Col lg={2}>
-                          <Form.Item label={'%'}>
+                          <Form.Item
+                            label={'%'}
+                            name={[
+                              'skills',
+                              index,
+                              'percentage',
+                            ]}
+                          >
                             <Input placeholder={'0'} />
                           </Form.Item>
                         </Col>
@@ -141,30 +215,46 @@ export default function UserForm() {
             <TabPane
               key={'bankAccount'}
               tab={'Dados bancários'}
+              forceRender
             >
               <Row gutter={24}>
                 <Col lg={8}>
-                  <Form.Item label={'Instituição'}>
+                  <Form.Item
+                    label={'Instituição'}
+                    name={['bankAccount', 'bankCode']}
+                  >
                     <Input placeholder={'260'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Agência'}>
+                  <Form.Item
+                    label={'Agência'}
+                    name={['bankAccount', 'agency']}
+                  >
                     <Input placeholder={'0001'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Conta sem dígito'}>
+                  <Form.Item
+                    label={'Conta sem dígito'}
+                    name={['bankAccount', 'accountNumber']}
+                  >
                     <Input placeholder={'12345'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Dígito'}>
+                  <Form.Item
+                    label={'Dígito'}
+                    name={['bankAccount', 'digit']}
+                  >
                     <Input placeholder={'1'} />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
-                  <Form.Item label={'Tipo de conta'}>
+                  <Form.Item
+                    label={'Tipo de conta'}
+                    name={['bankAccount', 'type']}
+                  >
                     <Select
                       placeholder={
                         'Selecione o tipo de conta'
@@ -182,6 +272,13 @@ export default function UserForm() {
               </Row>
             </TabPane>
           </Tabs>
+        </Col>
+        <Col lg={24}>
+          <Row justify={'end'}>
+            <Button type={'primary'} htmlType={'submit'}>
+              Cadastrar usuário
+            </Button>
+          </Row>
         </Col>
       </Row>
     </Form>
