e648096a9c564b80667eaab832adf6a5b32d3efe
Evandro Lima <evandro.lima@gat.digital>
2022-08-31 20:27:26 +0000

    Add middleware in reducer

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,9 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  isRejected,
+  Middleware,
+} from '@reduxjs/toolkit';
 import userReducer from './User.reducer';
+import { notification } from 'antd';
+
+const observeActions: Middleware =
+  () => (next) => (action) => {
+    if (isRejected(action)) {
+      notification.error({
+        message: action.error.message,
+      });
+    }
+
+    next(action);
+  };
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+  },
+  middleware: function (getDefaultMiddleware) {
+    return [...getDefaultMiddleware(), observeActions];
   },
 });
 
