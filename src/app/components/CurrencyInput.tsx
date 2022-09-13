ae2e63f326c2481a7053ab32b1a738280bd947af
Evandro Lima <evandro.lima@gat.digital>
2022-09-13 08:36:46 +0000

    Renderize editor currency value

--- src/app/components/CurrencyInput.tsx
+++ src/app/components/CurrencyInput.tsx
@@ -13,8 +13,6 @@ type CurrencyInputProps = Omit<
 export default function CurrencyInput(
   props: CurrencyInputProps
 ) {
-  const [inputValue, setInputValue] = useState('R$ 0,00');
-
   const convertValueToBrl = useCallback((value: number) => {
     return value.toLocaleString('pt-br', {
       style: 'currency',
@@ -23,6 +21,12 @@ export default function CurrencyInput(
       minimumFractionDigits: 2,
     });
   }, []);
+
+  const [inputValue, setInputValue] = useState(
+    typeof props.value === 'number'
+      ? convertValueToBrl(props.value)
+      : props.value
+  );
 
   return (
     <input
