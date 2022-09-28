d3dc172884ed80f0f4f47e0eac8dc8a85e47c7bf
Evandro Lima <evandro.lima@gat.digital>
2022-09-28 18:02:23 +0000

    Create category manager base

--- src/app/features/EntryCategoryManager.tsx
+++ src/app/features/EntryCategoryManager.tsx
@@ -1,3 +1,14 @@
+import { Button, Row, Table } from 'antd';
+import { CashFlow } from 'goodvandro-alganews-sdk';
+
 export default function EntryCategoryManager() {
-  return <div>todo: entry category manager</div>;
+  return (
+    <>
+      <Row justify={'space-between'}>
+        <Button>Atualizar Categorias</Button>
+        <Button>Adicionar Categoria</Button>
+      </Row>
+      <Table<CashFlow.CategorySummary> dataSource={[]} />
+    </>
+  );
 }
