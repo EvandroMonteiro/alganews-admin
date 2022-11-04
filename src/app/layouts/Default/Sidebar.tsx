b17a78e5a59961988cd7969a597d47687a6f1fee
Evandro Lima <evandro.lima@gat.digital>
2022-11-04 09:59:20 +0000

    Add drawer in small screen

--- src/app/layouts/Default/Sidebar.tsx
+++ src/app/layouts/Default/Sidebar.tsx
@@ -7,9 +7,22 @@ import {
   RiseOutlined,
   TableOutlined,
   UserOutlined,
+  MenuOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu, MenuProps } from 'antd';
+import {
+  Button,
+  Drawer,
+  DrawerProps,
+  Layout,
+  Menu,
+  MenuProps,
+  SiderProps,
+} from 'antd';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
+import { useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
+
 const { Sider } = Layout;
 
 const items: MenuProps['items'] = [
@@ -72,22 +85,65 @@ const items: MenuProps['items'] = [
 ];
 
 export default function DefaultLayoutSidebar() {
+  const { lg } = useBreakpoint();
+
   const location = useLocation();
 
+  const [show, setShow] = useState(false);
+
+  const SideBarWrapper: React.FC<{ children: React.ReactNode }> = useMemo(
+    () => (lg ? Sider : Drawer),
+    [lg]
+  );
+
+  const siderProps = useMemo((): SiderProps => {
+    return {
+      width: 200,
+      className: 'site-layout-background no-print',
+    };
+  }, []);
+
+  const drawerProps = useMemo((): DrawerProps => {
+    return {
+      visible: show,
+      closable: true,
+      title: (
+        <>
+          <img src={logo} alt={'logo alga news'} />
+        </>
+      ),
+      headerStyle: { height: 64 },
+      bodyStyle: { padding: 0 },
+      onClose() {
+        setShow(false);
+      },
+      placement: 'left',
+    };
+  }, [show]);
+
+  const sidebarProps = useMemo((): SiderProps | DrawerProps => {
+    return lg ? siderProps : drawerProps;
+  }, [lg, siderProps, drawerProps]);
+
   return (
-    <Sider
-      width={200}
-      className='site-layout-background no-print'
-      breakpoint='lg'
-      collapsedWidth='0'
-    >
-      <Menu
-        mode='inline'
-        defaultSelectedKeys={[location.pathname]}
-        defaultOpenKeys={[location.pathname.split('/')[1]]}
-        style={{ height: '100%', borderRight: 0 }}
-        items={items}
-      />
-    </Sider>
+    <>
+      {!lg && (
+        <Button
+          icon={<MenuOutlined />}
+          onClick={() => setShow(true)}
+          type={'text'}
+          style={{ position: 'fixed', top: 0, left: 0, height: 64, zIndex: 99 }}
+        />
+      )}
+      <SideBarWrapper {...sidebarProps}>
+        <Menu
+          mode='inline'
+          defaultSelectedKeys={[location.pathname]}
+          defaultOpenKeys={[location.pathname.split('/')[1]]}
+          style={{ height: '100%', borderRight: 0 }}
+          items={items}
+        />
+      </SideBarWrapper>
+    </>
   );
 }
